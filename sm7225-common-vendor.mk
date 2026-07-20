58bef6b73ce70cef34f6b7ac063588e2ad88b252
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-20 20:26:16 +0300

    fix

--- sm7225-common-vendor.mk
+++ sm7225-common-vendor.mk
@@ -146,6 +146,7 @@ PRODUCT_PACKAGES += \
     libSecureUILib \
     libStDrvInt \
     libVkLayer_q3dtools \
+    lib_SoundAlive_3DPosition_ver202 \
     libacdb-fts \
     libacdbloader \
     libacdbrtac \
@@ -229,6 +230,7 @@ PRODUCT_PACKAGES += \
     libperfgluelayer \
     libperfioctl \
     libperipheral_client \
+    libprofileparamstorage \
     libpvr \
     libqcbor \
     libqcc_file_agent \
@@ -273,6 +275,7 @@ PRODUCT_PACKAGES += \
     libsns_registry_skel \
     libsnsapi \
     libsnsdiaglog \
+    libspatializerparamstorage \
     libssc \
     libssc_default_listener \
     libssd \
